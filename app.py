842d1574dfbddbbc766870760b990ec715637aa4
Bernard Wilson <bernardwilson@Bernards-MBP.lan>
2025-04-23 09:15:59 -0400

    looping functionality

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-user_prompt = "Enter a todo:"
+# user_prompt = "Enter a todo:"
+
+# todos = []
+
+# while True:
+#     todo = input(user_prompt)
+#     print(todo.title())
+#     todos.append(todo)
+
 
 while True:
-    todo = input(user_prompt)
-    print(todo)
-    print("Next...")
+    name = input("Please enter your name: ")
+    print(name.title())
+    print("You've been added to the list of attendees, see you at 8!")
